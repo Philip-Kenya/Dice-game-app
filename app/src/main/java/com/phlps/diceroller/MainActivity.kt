95f500c8e77646fb98fcc6d3c61d1430fc618ab3
mwaphinju <phlpnjuguna@gmail.com>
2020-10-30 17:14:10 +0300

    app completed

--- app/src/main/java/com/phlps/diceroller/MainActivity.kt
+++ app/src/main/java/com/phlps/diceroller/MainActivity.kt
@@ -2,9 +2,8 @@ package com.phlps.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.Toast
 import com.phlps.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
@@ -14,8 +13,22 @@ class MainActivity : AppCompatActivity() {
         val view=binding.root
         setContentView(view)
         binding.rollButton.setOnClickListener{
-            Toast.makeText(this, "button clicked", Toast.LENGTH_LONG).show()
+            rollDice()
         }
 
+    }
+
+
+    private fun rollDice(){
+       val drawableResource= when(Random.nextInt(6)+1)
+       {
+           1->R.drawable.dice_1
+           2->R.drawable.dice_2
+           3->R.drawable.dice_3
+           4->R.drawable.dice_4
+           5->R.drawable.dice_5
+          else->R.drawable.dice_6
+       }
+        binding.diceImage.setImageResource(drawableResource)
     }
 }
